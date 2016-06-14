4dbdbb05cfba2c22e0be854189bf228532a45694
Sindre Sorhus <sindresorhus@gmail.com>
2016-06-14 19:25:13 +0200

    meta tweaks

--- test.js
+++ test.js
@@ -2,11 +2,11 @@ import test from 'ava';
 import execa from 'execa';
 
 test('main', async t => {
-	const ret = await execa('./cli.js', {cwd: __dirname});
-	t.is(ret.stdout.split('\n').length, 4);
+	const {stdout} = await execa('./cli.js');
+	t.is(stdout.split('\n').length, 4);
 });
 
 test('prop', async t => {
-	const ret = await execa('./cli.js', ['app'], {cwd: __dirname});
-	t.true(ret.stdout.length > 0);
+	const {stdout} = await execa('./cli.js', ['app']);
+	t.true(stdout.length > 0);
 });
